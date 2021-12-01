9a0339e46545246b165acf70aaa60f50e57e4c94
Mathieu <albatros69+github@gmail.com>
2021-12-01 14:05:59 +0100

    Commit day 1

---
+++ day-01/radar.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python
+
+import sys
+
+lines = []
+for line in sys.stdin:
+    lines.append(int(line.rstrip('\n')))
+
+count = 0
+for i in range(0, len(lines)-1):
+    count += int((lines[i+1]-lines[i])>0)
+print('Part 1:', count)
+
+sliding_windows = []
+tmp = lines
+for i in range(0, len(lines)-2):
+    a,b,c,*l = tmp
+    sliding_windows.append(a+b+c)
+    tmp = [b,c]+l
+count = 0
+for i in range(0, len(sliding_windows)-1):
+    count += int((sliding_windows[i+1]-sliding_windows[i])>0)
+print('Part 2:', count)
