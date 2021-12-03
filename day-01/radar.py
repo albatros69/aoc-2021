39a1ab2bade1c2d1a6a913eec63483284b2930d7
Mathieu <albatros69+github@gmail.com>
2021-12-03 22:31:00 +0100

    More pythonic solution

--- day-01/radar.py
+++ day-01/radar.py
@@ -11,12 +11,12 @@ for i in range(0, len(lines)-1):
     count += int((lines[i+1]-lines[i])>0)
 print('Part 1:', count)
 
-sliding_windows = []
-tmp = lines
-for i in range(0, len(lines)-2):
-    a,b,c,*l = tmp
-    sliding_windows.append(a+b+c)
-    tmp = [b,c]+l
+sliding_windows = [ sum(lines[i:i+3]) for i in range(0, len(lines)-2) ]
+# tmp = lines
+# for i in range(0, len(lines)-2):
+#     a,b,c,*l = tmp
+#     sliding_windows.append(a+b+c)
+#     tmp = [b,c]+l
 count = 0
 for i in range(0, len(sliding_windows)-1):
     count += int((sliding_windows[i+1]-sliding_windows[i])>0)
