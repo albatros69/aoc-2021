b74c281b80516b640cdb1266e764059ab49f7209
Mathieu <albatros69+github@gmail.com>
2021-12-08 10:12:10 +0100

    Cleaning...

--- day-08/display.py
+++ day-08/display.py
@@ -6,19 +6,6 @@ lines = []
 for line in sys.stdin:
     lines.append(line.rstrip('\n'))
 
-segments = {
-    'abcefg': 0 ,
-    'cf': 1 ,
-    'acdeg': 2 ,
-    'acdfg': 3 ,
-    'bcdf': 4 ,
-    'abdfg': 5 ,
-    'abdefg': 6 ,
-    'acf': 7 ,
-    'abcdefg': 8 ,
-    'abcdfg': 9,
-}
-
 count=0
 for l in lines:
     signals, displays = l.split(' | ')
